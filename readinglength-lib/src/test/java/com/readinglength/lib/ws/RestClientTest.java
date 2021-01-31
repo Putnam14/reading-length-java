cc0d85dd4c143ffc66e048c0512fb8c6d553156a
Putnam14 <putnambr76@gmail.com>
2021-01-31 15:18:36 -0700

    [#6] Add RestClient test

--- readinglength-lib/src/test/java/com/readinglength/lib/ws/RestClientTest.java
+++ readinglength-lib/src/test/java/com/readinglength/lib/ws/RestClientTest.java
@@ -4,21 +4,26 @@ package com.readinglength.lib.ws;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class RestClientTest {
     private static HttpEntity<String> httpEntity;
-    private RestTemplate mockRestTemplate;
     private RestClient restClient;
+
+    private RestTemplate mockRestTemplate;
+    private ResponseEntity<String> mockResponse;
 
     @BeforeAll
     static void setUp() {
@@ -30,8 +35,10 @@ class RestClientTest {
 
     @BeforeEach
     void setUpEach() {
+        mockResponse = (ResponseEntity<String>) mock(ResponseEntity.class);
         mockRestTemplate = mock(RestTemplate.class);
         restClient = new RestClient(mockRestTemplate, httpEntity);
+        when(mockResponse.getBody()).thenReturn("Success");
     }
 
 
@@ -39,5 +46,24 @@ class RestClientTest {
     void testIllegalState() {
         assertThrows(IllegalStateException.class, () -> restClient.get(Map.of()));
     }
+
+    @Test
+    void testQueryParams() {
+        when(mockRestTemplate
+                .exchange(
+                    "test?key1=value1&key2=value2+is+a+sentence",
+                    HttpMethod.GET,
+                    httpEntity,
+                    String.class))
+                .thenReturn(mockResponse);
+        restClient.setServerBaseUri("test");
+        
+        Map<String, String> queryParams = new LinkedHashMap<>();
+        queryParams.put("key1","value1");
+        queryParams.put("key2","value2 is a sentence");
+
+        assertEquals(mockResponse.getBody(), restClient.get(queryParams));
+    }
+
 
 }

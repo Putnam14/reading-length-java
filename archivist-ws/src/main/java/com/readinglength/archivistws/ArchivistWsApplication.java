54df02b8c6fd93c4a81d59ecc5686e77e9e00838
Putnam14 <putnambr76@gmail.com>
2021-06-28 20:59:34 -0600

    Deployability

--- archivist-ws/src/main/java/com/readinglength/archivistws/ArchivistWsApplication.java
+++ archivist-ws/src/main/java/com/readinglength/archivistws/ArchivistWsApplication.java
@@ -7,11 +7,19 @@ public class ArchivistWsApplication {
     public static void main(String[] args) {
         ArchivistComponent archivistComponent = DaggerArchivistComponent.create();
         Bookshelf bookshelf = archivistComponent.bookshelf();
-        Javalin app = Javalin.create().start(8080);
+        Javalin app = Javalin.create().start(getPort());
         app.get("/", bookshelf.index);
         app.get("/books/isbn", bookshelf.queryBookByIsbn);
         app.post("/books/insert", bookshelf.insertBook);
         app.get("/isbns/title", bookshelf.queryIsbnByTitle);
         app.get("/isbns", bookshelf.isbnExists);
+    }
+
+    private static int getPort() {
+        String port = System.getenv("PORT");
+        if (port == null) {
+            return 8080;
+        }
+        return Integer.valueOf(port);
     }
 }
